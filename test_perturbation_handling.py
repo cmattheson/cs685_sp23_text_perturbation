089e43ea0aa4e76a2bff845812c9caaf222fdd98
cmattheson6 <cmattheson6@gmail.com>
2023-04-23 21:00:44 -0400

    fully built data ingestion process

---
+++ test_perturbation_handling.py
@@ -0,0 +1,33 @@
+
+import os
+from src.perturbation_calculator import PerturbationCalculator
+
+
+def main():
+
+    calc = PerturbationCalculator(log_directory=f'{os.path.dirname(__file__)}/logs')
+    calc.check_for_text_pair_perturbations(
+        'the quick brown fox jumped over the lazy dog',
+        'the quick brown fox jumped over the lazy dog'
+    )
+    calc.check_for_text_pair_perturbations(
+        'the quick brown fox jumped over the lazy dog',
+        'the quickkkkk brown foix julmped ov er the lazy doggo'
+    )
+    calc.check_for_text_pair_perturbations(
+        'the quick brown fox jumped over the lazy dog',
+        'th quik brwn fox jumped over the lazy do'
+    )
+    calc.check_for_text_pair_perturbations(
+        'the quick brown fox jumped over the lazy dog',
+        'tha qu ck briwn fix jumped over the lazy dog'
+    )
+    calc.check_for_text_pair_perturbations(
+        'the quick brown fox jumped over the lazy dog',
+        'the qucik brown foxj umped oevr the lazy dog'
+    )
+    calc.store_results()
+
+
+if __name__ == '__main__':
+    main()
