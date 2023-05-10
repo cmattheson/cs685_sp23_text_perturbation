8955ab080f5e6b57891160ef6aa119b43f739a46
William Eliot <weliot@umass.edu>
2023-05-09 20:51:40 -0400

    removed PerturbedSequenceDataset2 to PerturbedSequenceDataset

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -32,29 +32,59 @@ class CustomDataset(Dataset):
 
 
 class PerturbedSequenceDataset(Dataset):
-    def __init__(self, data, log_directory, perturbation_rate=1.0, perturbation_rate_per_char=0.15):
-        self.handler = TextPerturbationHandler(log_directory=log_directory)
-        self.data = data
-        self.perturbation_rate = perturbation_rate
-        self.perturbation_rate_per_char = perturbation_rate_per_char
-
-    def __getitem__(self, idx):
-        itm = self.data[idx]
-        label = itm[1]
-        text = self.handler.perturb_string(itm[0])
-        return text, label
-
-    def __len__(self):
-        return len(self.data)
-
-class PerturbedSequenceDataset2(Dataset):
     """"
     """
     def __init__(self,
                  data,
                  labels,
-                 log_directory='../../logs/character_perturbation',
-                 word_perturbation_rate=0.15,
+                 log_directory='./logs/character_perturbation',
+                 word_perturbation_rate=0.0,
+                 perturb_characters=True,
+                 tokenizer=BertTokenizer.from_pretrained('bert-base-uncased'),
+                 require_elmo_ids=True):
+        self.handler = TextPerturbationHandler(log_directory=log_directory)
+        self.data = data
+        self.labels = labels
+        self.perturb_characters = perturb_characters
+        self.word_perturbation_rate = word_perturbation_rate
+        self.tokenizer = tokenizer
+        self.require_elmo_ids = require_elmo_ids
+        self.max_word_length = 50
+        self.max_sentence_length = 50 # restrict to 50 for now so we don't run out of vram :(
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        text = self.data[idx]
+
+        if self.word_perturbation_rate > 0:
+            text = sentence_pertube(text, self.word_perturbation_rate)
+        if self.perturb_characters:
+            text = self.handler.perturb_string(text)
+        if self.tokenizer:
+            encoded_input = self.tokenizer(text, return_tensors='pt')
+            input_ids = encoded_input['input_ids'].squeeze(0)
+            input_ids = pad(input_ids, (0, self.max_sentence_length - input_ids.shape[0]), value=0)
+            attention_mask = encoded_input['attention_mask'].squeeze(0)
+            attention_mask = pad(attention_mask, (0, self.max_sentence_length - attention_mask.shape[0]), value=0)
+            if self.require_elmo_ids:
+                elmo_input_ids = batch_to_ids([self.tokenizer.tokenize(text)]).squeeze(0)
+                elmo_input_ids = pad(elmo_input_ids, (0, self.max_word_length - elmo_input_ids.shape[1], 0,
+                                                      self.max_sentence_length - elmo_input_ids.shape[0]), value=0)
+
+                return input_ids, elmo_input_ids, attention_mask, label
+            else:
+                return input_ids, attention_mask, label
+
+    def __len__(self):
+        return len(self.data)
+
+class PerturbedSequenceDataset(Dataset):
+    """"
+    """
+    def __init__(self,
+                 data,
+                 labels,
+                 log_directory='./logs/character_perturbation',
+                 word_perturbation_rate=0.0,
                  perturb_characters=True,
                  tokenizer=BertTokenizer.from_pretrained('bert-base-uncased'),
                  require_elmo_ids=True):
